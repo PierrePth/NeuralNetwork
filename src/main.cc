95cb244fe1c0923d03de5ea519a6d3ea30f41949
Pierre-Adrien Ponthieu <pierre-adrien.ponthieu@epita.fr>
2018-03-14 00:47:16 +0100

    main.cc: adding a little documentation to functions

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,13 @@
 #include "tools.hh"
 #include "neural_net.hh"
 
+/** \name Feed forward propagation function
+
+    \brief This function updates the neurons by calculating
+    the sigmoid of the logit
+
+    \param The neural network
+*/
 void feed_forward(NeuralNet& nn)
 {
   for (size_t i = 0; i < nn.hidden_nb_get(); ++i)
@@ -28,7 +35,22 @@ void feed_forward(NeuralNet& nn)
   } 
 }
 
+/** \name Trains the Neural network
 
+    \brief This function first calcul the output error.
+    Then, it computes the gradients of the weights that
+    goes from the hidden layer to the output layer by
+    applying the formula of the partial derivative of
+    the error function E thanks to the weights W.
+    It does the same thing with the first matrix
+    of weights, this time adding adding the sum of product
+    of each weight from the w2 matrix in order to retropropagate
+    the error from the output correctly. Finally, we update the
+    weights of the 2 matrix simply by multiplying the gradient by
+    the learning rate.
+
+    \param The neural network and the expected output
+*/
 void train(NeuralNet& nn, std::vector<double> out_data)
 {
   for (size_t i = 0; i < out_data.size(); ++i)
@@ -62,7 +84,8 @@ void train(NeuralNet& nn, std::vector<double> out_data)
     for (size_t j = 0; j < nn.hidden_nb_get(); ++j)
     {
       double old_weight = nn.w2_get()[i][j];
-      nn.w2_set(i, j, old_weight - nn.learning_rate_get() * nn.output_gradient_get()[i][j]);
+      nn.w2_set(i, j, old_weight - nn.learning_rate_get()
+                * nn.output_gradient_get()[i][j]);
     }
   }
 
@@ -71,11 +94,19 @@ void train(NeuralNet& nn, std::vector<double> out_data)
     for (size_t j = 0; j < nn.input_nb_get(); ++j)
     {
       double old_weight = nn.w1_get()[i][j];
-      nn.w1_set(i, j, old_weight - nn.learning_rate_get() * nn.hidden_gradient_get()[i][j]);
+      nn.w1_set(i, j, old_weight - nn.learning_rate_get()
+                * nn.hidden_gradient_get()[i][j]);
     }
   }
 }
 
+/** \name main function
+
+    \brief This function parse the data_sets files
+    and run a SGD which trains the network the files.
+
+    \param Usual parameters
+*/
 int main(int argc, char **argv)
 {
   if (argc < 7)
